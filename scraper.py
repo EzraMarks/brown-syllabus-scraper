5205cb7b3b308ba99f7a00788134426d58ce4870
Ezra Marks <ezrablankmarks@gmail.com>
2022-05-04 20:44:18 -0400

    Complete web scraping first draft

--- scraper.py
+++ scraper.py
@@ -1,58 +1,80 @@
-from json.tool import main
-from turtle import pd
+from time import sleep
 import turtle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-
-def log_in(driver):
-    brown_username = os.environ["BROWN_USER"]
-    brown_password = os.environ["BROWN_PASS"]
-    main_window = driver.window_handles[0]
-    login_window = driver.window_handles[1]
-    driver.switch_to.window(login_window)
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "username")))
-    username_field = driver.find_element(By.ID, "username")
-    username_field.send_keys(brown_username)
-    password_field = driver.find_element(By.ID, "password")
-    password_field.send_keys(brown_password)
-    login_button = driver.find_element(By.XPATH, "//button[contains(text(),'Log In')]")
-    login_button.click()
-    driver.switch_to.window(main_window)
-    # Manually complete Duo 2FA
-    WebDriverWait(driver, 60).until(EC.number_of_windows_to_be(1))
+import sqlite3
 
 service = Service("./chromedriver")
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://cab.brown.edu")
 
-# Select first department on the list
-department_dropdown = driver.find_element(By.ID, "crit-dept")
-department_selector = Select(department_dropdown)
-department_selector.select_by_index(1)
-find_courses_form = driver.find_element(By.ID, "search-form")
-find_courses_form.submit()
+# Makes CAB stop complaining that you need to narrow down the search options
+# Click "Exclude Times" option
+exclude_times_checkbox = driver.find_element(By.ID, "crit-custom-meeting-pattern")
+exclude_times_checkbox.click()
+# Click on first cell to exclude that time
+WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME, "select-grid__block")))
+sleep(5)
+exclude_times_checkbox.send_keys(Keys.TAB)
+exclude_times_checkbox.send_keys(Keys.TAB)
+exclude_times_checkbox.send_keys(Keys.TAB)
+selected_block = driver.find_element(By.CLASS_NAME, "select-grid__block--focus")
+selected_block.send_keys(Keys.ENTER)
+# Click "Done"
+WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME, "meet-pat-btn-search")))
+done_button = driver.find_element(By.CLASS_NAME, "meet-pat-btn-search")
+done_button.click()
+# Click "Find Courses" button to search
+# sleep(5)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search-button")))
+search_button = driver.find_element(By.ID, "search-button")
+search_button.click()
+
 # Wait until list of courses have loaded
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "result--group-start")))
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "result--group-start")))
 results = driver.find_elements(By.CLASS_NAME, "result--group-start")
-# Click on the 4th element of the list
-results[4].click()
-# Click on the syllabus
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h3[contains(text(),'Course Resources')]")))
-syllabus_link = driver.find_element(By.LINK_TEXT, "Class Syllabus")
-syllabus_link.click()
 
-log_in(driver)
+# Create connection to database
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
 
-# Download syllabus
+# Delete tables if they exist
+c.execute('DROP TABLE IF EXISTS "syllabi";')
+
+# DONE: Create tables in the database and add data to it. REMEMBER TO COMMIT
+c.execute('''
+    CREATE TABLE syllabi (
+        course_code VARCHAR(255) NOT NULL PRIMARY KEY,
+        department VARCHAR(255),
+        course_number VARCHAR(255),
+        course_title VARCHAR(255),
+        syllabus_link VARCHAR(255)
+    );
+''')
+conn.commit()
+
+for result in results:
+    course_code = result.find_element(By.TAG_NAME, "a").get_attribute("data-group")[5:]
+    [department, course_number] = course_code.split(" ")
+    course_title = result.find_element(By.CLASS_NAME, "result__title").text
+    syllabus_link = f"https://coursetools.brown.edu/syllabus/{department}:{course_number}:2022-Spring:S01"
+
+    c.execute('INSERT INTO syllabi VALUES (?, ?, ?, ?, ?)',
+        (course_code, department, course_number, course_title, syllabus_link))
+        
+    print(course_code)
+
+conn.commit()
 
 
-
-WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Log In')]")))
-
+conn.close()
 quit()
+
+
+
+
